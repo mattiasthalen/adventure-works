32baabca8d503e7d325e9927fa6441cb7e2583d3
Mattias Thalén <bitter-polders0x@icloud.com>
2025-03-05 14:18:50 +0100

    Update bridge template

--- models/silver/uss_bridge__products.sql
+++ models/silver/uss_bridge__products.sql
@@ -5,61 +5,20 @@ MODEL (
   ),
   tags uss,
   grain (_pit_hook__product),
-  references (_pit_hook__product_subcategory, _pit_hook__product_category)
+  references (_hook__product_subcategory, _hook__reference__product_model)
 );
 
-WITH bridge AS (
-  SELECT
-    'products' AS stage,
-    bag__adventure_works__products._pit_hook__product,
-    bag__adventure_works__product_subcategories._pit_hook__product_subcategory,
-    bag__adventure_works__product_categories._pit_hook__product_category,
-    GREATEST(
-      bag__adventure_works__products.product__record_loaded_at,
-      bag__adventure_works__product_subcategories.product_subcategory__record_loaded_at,
-      bag__adventure_works__product_categories.product_category__record_loaded_at
-    ) AS bridge__record_loaded_at,
-    GREATEST(
-      bag__adventure_works__products.product__record_updated_at,
-      bag__adventure_works__product_subcategories.product_subcategory__record_updated_at,
-      bag__adventure_works__product_categories.product_category__record_updated_at
-    ) AS bridge__record_updated_at,
-    GREATEST(
-      bag__adventure_works__products.product__record_valid_from,
-      bag__adventure_works__product_subcategories.product_subcategory__record_valid_from,
-      bag__adventure_works__product_categories.product_category__record_valid_from
-    ) AS bridge__record_valid_from,
-    LEAST(
-      bag__adventure_works__products.product__record_valid_to,
-      bag__adventure_works__product_subcategories.product_subcategory__record_valid_to,
-      bag__adventure_works__product_categories.product_category__record_valid_to
-    ) AS bridge__record_valid_to
-  FROM silver.bag__adventure_works__products
-  LEFT JOIN silver.bag__adventure_works__product_subcategories
-    ON bag__adventure_works__products._hook__product_subcategory = bag__adventure_works__product_subcategories._hook__product_subcategory
-    AND bag__adventure_works__products.product__record_valid_from <= bag__adventure_works__product_subcategories.product_subcategory__record_valid_to
-    AND bag__adventure_works__products.product__record_valid_to >= bag__adventure_works__product_subcategories.product_subcategory__record_valid_from
-  LEFT JOIN silver.bag__adventure_works__product_categories
-    ON bag__adventure_works__product_subcategories._hook__product_category = bag__adventure_works__product_categories._hook__product_category
-    AND bag__adventure_works__products.product__record_valid_from <= bag__adventure_works__product_categories.product_category__record_valid_to
-    AND bag__adventure_works__products.product__record_valid_to >= bag__adventure_works__product_categories.product_category__record_valid_from
-), final AS (
-  SELECT
-    *,
-    bridge__record_valid_to = '9999-12-31 23:59:59'::TIMESTAMP AS bridge__is_current_record
-  FROM bridge
-)
-
 SELECT
-  stage::BLOB,
-  _pit_hook__product::BLOB,
-  _pit_hook__product_subcategory::BLOB,
-  _pit_hook__product_category::BLOB,
-  bridge__record_loaded_at::TIMESTAMP,
-  bridge__record_updated_at::TIMESTAMP,
-  bridge__record_valid_from::TIMESTAMP,
-  bridge__record_valid_to::TIMESTAMP,
-  bridge__is_current_record::TEXT
-FROM final
+  'products' AS peripheral,
+  _hook__product::BLOB,
+  _hook__product_subcategory::BLOB,
+  _hook__reference__product_model::BLOB,
+  product__record_loaded_at::TIMESTAMP AS bridge__record_loaded_at,
+  product__record_updated_at::TIMESTAMP AS bridge__record_updated_at,
+  product__record_version::TEXT AS bridge__record_version,
+  product__record_valid_from::TIMESTAMP AS bridge__record_valid_from,
+  product__record_valid_to::TIMESTAMP AS bridge__record_valid_to,
+  product__is_current_record::TEXT AS bridge__is_current_record
+FROM silver.bag__adventure_works__products
 WHERE 1 = 1
 AND bridge__record_updated_at BETWEEN @start_ts AND @end_ts
