9065ddcdd3778fb0963aa00fb69ff1577276ae3c
Mattias Thalén <bitter-polders0x@icloud.com>
2025-03-13 12:06:48 +0100

    Move shell scripts to uv

--- elt.sh
+++
@@ -1,3 +0,0 @@
-uv run ./pipelines/adventure_works.py prod
-uv run sqlmesh plan prod
-uv run ./pipelines/duckdb_to_iceberg.py
--- init.sh
+++
@@ -1,3 +0,0 @@
-uv run ./pipelines/adventure_works.py prod
-uv run sqlmesh run prod
-uv run ./pipelines/duckdb_to_iceberg.py
