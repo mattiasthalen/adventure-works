1402535d3ed97c290770d1d191ad94df0eec9824
Mattias Thalén <bitter-polders0x@icloud.com>
2025-03-28 14:18:07 +0100

    Add ghost records to peripheral blueprint model

--- models/dar__peripheral__blueprint.py
+++ models/dar__peripheral__blueprint.py
@@ -30,6 +30,41 @@ def entrypoint(evaluator: MacroEvaluator) -> str | exp.Expression:
     column_descriptions = evaluator.var("column_descriptions")
     columns = evaluator.var("columns")
     hook_name = evaluator.var("hook_name")
+    grain = evaluator.var("grain")
+
+    cte__source = exp.select(*columns).from_(f"dab.{hook_name}")
+
+    # Create ghost record
+    ghost_columns = [exp.Literal.string("ghost_record").as_(grain)]
+    for column, data_type in column_data_types.items():
+
+        if column == grain:
+            continue
+
+        elif data_type == "text":
+            ghost_column = exp.Literal.string("N/A")
+        
+        elif column.endswith(("__record_loaded_at", "__record_updated_at", "__record_valid_from")):
+            ghost_column = exp.cast(exp.Literal.string("1970-01-01 00:00:00"), exp.DataType.build("timestamp"))
+        
+        elif column.endswith("__record_valid_to"):
+            ghost_column = exp.cast(exp.Literal.string("9999-12-31 23:59:59"), exp.DataType.build("timestamp"))
+        
+        elif column.endswith("__record_version"):
+            ghost_column = exp.Literal.number(0)
+        
+        elif column.endswith("__is_current_record"):
+            ghost_column = exp.true()
+        
+        else:
+            ghost_column = exp.Null()
+        
+        ghost_columns.append(ghost_column.as_(column))
+
+    cte__ghost = exp.select(*ghost_columns)
+
+    # Add ghost record
+    cte_union = exp.union(cte__source, cte__ghost)
 
     # Explicit cast and commenting
     casted_columns = []
@@ -44,6 +79,12 @@ def entrypoint(evaluator: MacroEvaluator) -> str | exp.Expression:
         
         casted_columns.append(casted_column)
 
-    sql = exp.select(*casted_columns).from_(f"dab.{hook_name}")
+    sql = (
+        exp.select(*casted_columns)
+        .from_("cte_union")
+        .with_("cte__source", as_=cte__source)
+        .with_("cte__ghost", as_=cte__ghost)
+        .with_("cte_union", as_=cte_union)
+    )
 
     return sql
